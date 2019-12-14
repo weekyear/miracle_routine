0299ba87b83c46a0b3f46bb8d8c2dde356a4c8b1
JWeeKyear <jwnsgus@gmail.com>
2019-12-14 14:19:09 +0900

    Initial Commit

---
+++ miracle_routine/miracle_routine/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using miracle_routine.Services;
+using miracle_routine.Views;
+
+namespace miracle_routine
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new NavigationPage(new ItemsPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ miracle_routine/miracle_routine/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using miracle_routine.Models;
+
+namespace miracle_routine.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
