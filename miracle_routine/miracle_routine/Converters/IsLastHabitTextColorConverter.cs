293c81dec5b9f8e579bcf0f1786b6b391c751ca3
JWeeKyear <jwnsgus@gmail.com>
2020-01-02 16:01:31 +0900

    DoneButton 대신 IsLastHabitTextColorConverter 사용해서 TextColor 유동적으로 바꿔줌

---
+++ miracle_routine/miracle_routine/Converters/IsLastHabitTextColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace miracle_routine.Converters
+{
+    public class IsLastHabitTextColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return BooleanToColor(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        private object BooleanToColor(object value)
+        {
+            var isNotLastHabit = (bool)value;
+            if (isNotLastHabit)
+            {
+                return (Color)App.Current.Resources["PrimaryDark"];
+            }
+            else
+            {
+                return (Color)App.Current.Resources["Accent"];
+            }
+        }
+    }
+}
